f021c6cf89c6919c02fbbec126dc1308c9afa608
SlimNate <slimnatekc@gmail.com>
2023-12-10 14:49:16 -0600

    day 4 part 2

--- 04/main.go
+++ 04/main.go
@@ -29,7 +29,7 @@ func (s IntSlice) Contains(v int) bool {
 func ParseCard(line string) Card {
 	s1 := strings.Split(line, ": ")
 	s2 := strings.Split(s1[0], " ")
-	cardNo, _ := strconv.Atoi(s2[1])
+	cardNo, _ := strconv.Atoi(s2[len(s2)-1])
 
 	s3 := strings.Split(s1[1], " | ")
 	s4 := strings.Split(s3[0], " ")
@@ -82,6 +82,21 @@ func (c Card) Value() int {
 	return int(math.Pow(2, math.Max(0, float64(wnc-1))))
 }
 
+func (c Card) Copies(cards []Card, depth int) []Card {
+	// fmt.Printf("depth: %v\n", depth)
+	var copies []Card
+	wnc := c.WinningNumberCount()
+
+	// fmt.Printf("card: %+v\n", c)
+	for _, copy := range cards[c.CardNumber : c.CardNumber+wnc] {
+		// fmt.Printf("copy: %v\n", copy)
+		copies = append(copies, copy)
+		copies = append(copies, copy.Copies(cards, depth+1)...)
+	}
+	// fmt.Printf("copies: %v\n", copies)
+	return copies
+}
+
 func main() {
 	file, _ := os.Open("input.txt")
 	scanner := bufio.NewScanner(file)
@@ -98,5 +113,16 @@ func main() {
 	}
 
 	fmt.Printf("totalValue: %v\n", totalValue)
+
+	var cardsAfterCopy []Card
+
+	for _, card := range cards {
+		cardsAfterCopy = append(cardsAfterCopy, card)
+		cardsAfterCopy = append(cardsAfterCopy, card.Copies(cards, 0)...)
+		// fmt.Printf("cardsAfterCopy: %v\n", cardsAfterCopy)
+	}
+
+	// fmt.Printf("final cardsAfterCopy: %v\n", cardsAfterCopy)
+	fmt.Printf("len(cardsAfterCopy): %v\n", len(cardsAfterCopy))
 
 }

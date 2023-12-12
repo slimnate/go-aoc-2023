059a4f3644f2d273ad7810b9298c9a511780a83e
SlimNate <slimnatekc@gmail.com>
2023-12-12 13:34:36 -0600

    day 6 part 2

--- 06/main.go
+++ 06/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strconv"
+	"strings"
 	"utils"
 )
 
@@ -33,6 +35,18 @@ func MaxButtonTimeToWin(raceTime int, record int) int {
 	return -1
 }
 
+func WaysToWin(raceTime int, record int) int {
+	minWinTime := MinButtonTimeToWin(raceTime, record)
+	maxWinTime := MaxButtonTimeToWin(raceTime, record)
+
+	winningOptionCount := maxWinTime - minWinTime + 1
+
+	fmt.Printf("minWinTime: %v\n", minWinTime)
+	fmt.Printf("maxWinTime: %v\n", maxWinTime)
+	fmt.Printf("winningOptionCount: %v\n", winningOptionCount)
+	return winningOptionCount
+}
+
 func main() {
 	lines, _ := utils.Lines("input.txt")
 
@@ -46,19 +60,24 @@ func main() {
 
 	winningOptionsProduct := 1
 
-	// for each race, calculate the min and max button times that will beat the record
+	// Part 1 - for each race, calculate the min and max button times that will beat the record
 	for i, time := range times {
-		minWinTime := MinButtonTimeToWin(time, records[i])
-		maxWinTime := MaxButtonTimeToWin(time, records[i])
-
-		winningOptionCount := maxWinTime - minWinTime + 1
-
-		fmt.Printf("minWinTime: %v\n", minWinTime)
-		fmt.Printf("maxWinTime: %v\n", maxWinTime)
-		fmt.Printf("winningOptionCount: %v\n", winningOptionCount)
-
+		winningOptionCount := WaysToWin(time, records[i])
 		winningOptionsProduct *= winningOptionCount
 	}
 
 	fmt.Printf("winningOptionsProduct: %v\n", winningOptionsProduct)
+
+	// Part 2 - same problem, but parsed differently, so there is only one race
+	timesStr := strings.Split(lines[0], ":")[1]
+	timesStr = strings.ReplaceAll(timesStr, " ", "")
+	time, _ := strconv.Atoi(timesStr)
+
+	recrodsStr := strings.Split(lines[1], ":")[1]
+	recrodsStr = strings.ReplaceAll(recrodsStr, " ", "")
+	record, _ := strconv.Atoi(recrodsStr)
+
+	winningOptionsCount := WaysToWin(time, record)
+
+	fmt.Printf("winningOptionsCount: %v\n", winningOptionsCount)
 }
